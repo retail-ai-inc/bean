93d0c65e55b62e7114c5441cef9a32e530af7659
Tanvir Shahid <tanvir@retail-ai.jp>
2023-02-10 23:23:29 +0900

    Update documentation.

--- helpers/jitter.go
+++ helpers/jitter.go
@@ -7,8 +7,33 @@ import (
 	"time"
 )
 
-// JitterBackoff Return capped exponential backoff with jitter
+var rnd = newRnd()
+var rndMu sync.Mutex
+
+// JitterBackoff Return capped exponential backoff with jitter. It is useful for http client when you want to retry request.
 // http://www.awsarchitectureblog.com/2015/03/backoff.html
+
+// An example use case:
+
+// for i := 0; i <= retryCount; i++ {
+//     resp, err := http.Get("https://retail-ai.jp")
+//     if err == nil {
+//       return nil
+//     }
+
+//     // Don't need to wait when no retries left.
+//     if i == retryCount {
+//       return err
+//     }
+
+//     waitTime := helpers.JitterBackoff(time.Duration(100) * time.Millisecond, time.Duration(2000) * time.Millisecond, i)
+
+//     select {
+//     case <-time.After(waitTime):
+//     case <-c.Done():
+//       return c.Err()
+//     }
+// }
 func JitterBackoff(min, max time.Duration, attempt int) time.Duration {
 	base := float64(min)
 	capLevel := float64(max)
@@ -23,9 +48,6 @@ func JitterBackoff(min, max time.Duration, attempt int) time.Duration {
 
 	return result
 }
-
-var rnd = newRnd()
-var rndMu sync.Mutex
 
 func randDuration(center time.Duration) time.Duration {
 	rndMu.Lock()
