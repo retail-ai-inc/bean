fe7391e268e0d75c4d28e566ee0f9d8fac58829d
钟大陆 <10188142zhong_dalu@cn.tre-inc.com>
2023-06-16 10:32:03 +0800

    single flight test coverage 100%

--- helpers/singleflight_test.go
+++ helpers/singleflight_test.go
@@ -80,7 +80,7 @@ func TestSingleDoChan(t *testing.T) {
 					time.Sleep(time.Second * 2)
 					return nil, errors.New("some error")
 				},
-				retry: 2,
+				retry: 0,
 				ttl:   []time.Duration{time.Second},
 			},
 			wantData: nil,
@@ -92,9 +92,9 @@ func TestSingleDoChan(t *testing.T) {
 				ctx: context.Background(),
 				key: "test2",
 				call: func() (any, error) {
-					return 123, errors.New("some error")
+					return 0, errors.New("some error")
 				},
-				retry: 2,
+				retry: 0,
 				ttl:   nil,
 			},
 			wantData: nil,
@@ -108,7 +108,7 @@ func TestSingleDoChan(t *testing.T) {
 				call: func() (any, error) {
 					panic("cause panic here")
 				},
-				retry: 2,
+				retry: 0,
 				ttl:   nil,
 			},
 			wantData: nil,
@@ -123,6 +123,21 @@ func TestSingleDoChan(t *testing.T) {
 					time.Sleep(time.Second * 2)
 					return "data", nil
 				},
+				retry: 0,
+				ttl:   nil,
+			},
+			wantData: nil,
+			wantErr:  true,
+		},
+		{
+			name: "error timeout",
+			args: args{
+				ctx: timeoutCtx,
+				key: "test5",
+				call: func() (any, error) {
+					time.Sleep(time.Millisecond * 500)
+					return "", errors.New("some error")
+				},
 				retry: 2,
 				ttl:   nil,
 			},
@@ -134,19 +149,17 @@ func TestSingleDoChan(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			ctx, cancel := context.WithCancel(tt.args.ctx)
 			defer cancel()
-			gotData, err := SingleDoChan[T](ctx, tt.args.key, func() (T, error) {
-				res, err := tt.args.call()
-				if err != nil {
-					return nil, err
-				}
-				return res, nil
-			}, tt.args.retry, tt.args.ttl...)
+			gotData, err := SingleDoChan(ctx, tt.args.key, tt.args.call, tt.args.retry, tt.args.ttl...)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("SingleDoChan() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(gotData, tt.wantData) {
 				t.Errorf("SingleDoChan() gotData = %v, want %v", gotData, tt.wantData)
+			}
+			if tt.args.retry > 0 && err != nil {
+				// waiting for goroutine finish
+				time.Sleep(2 * time.Second)
 			}
 		})
 	}
