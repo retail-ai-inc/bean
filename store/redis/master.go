83eb7c8788f9b5d0e27b353ec8bef3af8429e305
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2024-01-15 17:37:04 +0900

    remove unnecessary args from master cache

--- store/redis/master.go
+++ store/redis/master.go
@@ -35,21 +35,21 @@ const masterID uint64 = 0
 // MasterCache provides functions for master redis db.
 type MasterCache interface {
 	GetJSON(c context.Context, key string, dst interface{}) (bool, error)
-	GetString(c context.Context, companyID, subsidiaryID uint64, key string) (string, error)
-	MGet(c context.Context, companyID, subsidiaryID uint64, keys ...string) ([]interface{}, error)
-	HGet(c context.Context, companyID, subsidiaryID uint64, key string, field string) (string, error)
-	LRange(c context.Context, companyID, subsidiaryID uint64, key string, start, stop int64) ([]string, error)
-	SMembers(c context.Context, companyID, subsidiaryID uint64, key string) ([]string, error)
-	SIsMember(c context.Context, companyID, subsidiaryID uint64, key string, element interface{}) (bool, error)
-	SetJSON(c context.Context, companyID, subsidiaryID uint64, key string, data interface{}, ttl time.Duration) error
-	SetString(c context.Context, companyID, subsidiaryID uint64, key string, data string, ttl time.Duration) error
-	HSet(c context.Context, companyID, subsidiaryID uint64, key string, field string, data interface{}, ttl time.Duration) error
-	RPush(c context.Context, companyID, subsidiaryID uint64, key string, valueList []string) error
-	IncrementValue(c context.Context, companyID, subsidiaryID uint64, key string) error
-	SAdd(c context.Context, companyID, subsidiaryID uint64, key string, elements interface{}) error
-	SRem(c context.Context, companyID, subsidiaryID uint64, key string, elements interface{}) error
-	DelKey(c context.Context, companyID, subsidiaryID uint64, keys ...string) error
-	Expire(c context.Context, companyID, subsidiaryID uint64, key string, ttl time.Duration) error
+	GetString(c context.Context, key string) (string, error)
+	MGet(c context.Context, keys ...string) ([]interface{}, error)
+	HGet(c context.Context, key string, field string) (string, error)
+	LRange(c context.Context, key string, start, stop int64) ([]string, error)
+	SMembers(c context.Context, key string) ([]string, error)
+	SIsMember(c context.Context, key string, element interface{}) (bool, error)
+	SetJSON(c context.Context, key string, data interface{}, ttl time.Duration) error
+	SetString(c context.Context, key string, data string, ttl time.Duration) error
+	HSet(c context.Context, key string, field string, data interface{}, ttl time.Duration) error
+	RPush(c context.Context, key string, valueList []string) error
+	IncrementValue(c context.Context, key string) error
+	SAdd(c context.Context, key string, elements interface{}) error
+	SRem(c context.Context, key string, elements interface{}) error
+	DelKey(c context.Context, keys ...string) error
+	Expire(c context.Context, key string, ttl time.Duration) error
 	Pipelined(c context.Context, fn func(redis.Pipeliner) error) ([]redis.Cmder, error)
 }
 
@@ -79,63 +79,63 @@ func (m *masterCache) GetJSON(c context.Context, key string, dst interface{}) (b
 	return m.cache.GetJSON(c, masterID, key, dst)
 }
 
-func (m *masterCache) GetString(c context.Context, companyID, subsidiaryID uint64, key string) (string, error) {
+func (m *masterCache) GetString(c context.Context, key string) (string, error) {
 	return m.cache.GetString(c, masterID, key)
 }
 
-func (m *masterCache) MGet(c context.Context, companyID, subsidiaryID uint64, keys ...string) ([]interface{}, error) {
+func (m *masterCache) MGet(c context.Context, keys ...string) ([]interface{}, error) {
 	return m.cache.MGet(c, masterID, keys...)
 }
 
-func (m *masterCache) HGet(c context.Context, companyID, subsidiaryID uint64, key string, field string) (string, error) {
+func (m *masterCache) HGet(c context.Context, key string, field string) (string, error) {
 	return m.cache.HGet(c, masterID, key, field)
 }
 
-func (m *masterCache) LRange(c context.Context, companyID, subsidiaryID uint64, key string, start, stop int64) ([]string, error) {
+func (m *masterCache) LRange(c context.Context, key string, start, stop int64) ([]string, error) {
 	return m.cache.LRange(c, masterID, key, start, stop)
 }
 
-func (m *masterCache) SMembers(c context.Context, companyID, subsidiaryID uint64, key string) ([]string, error) {
+func (m *masterCache) SMembers(c context.Context, key string) ([]string, error) {
 	return m.cache.SMembers(c, masterID, key)
 }
 
-func (m *masterCache) SIsMember(c context.Context, companyID, subsidiaryID uint64, key string, element interface{}) (bool, error) {
+func (m *masterCache) SIsMember(c context.Context, key string, element interface{}) (bool, error) {
 	return m.cache.SIsMember(c, masterID, key, element)
 }
 
-func (m *masterCache) SetJSON(c context.Context, companyID, subsidiaryID uint64, key string, data interface{}, ttl time.Duration) error {
+func (m *masterCache) SetJSON(c context.Context, key string, data interface{}, ttl time.Duration) error {
 	return m.cache.SetJSON(c, masterID, key, data, ttl)
 }
 
-func (m *masterCache) SetString(c context.Context, companyID, subsidiaryID uint64, key string, data string, ttl time.Duration) error {
+func (m *masterCache) SetString(c context.Context, key string, data string, ttl time.Duration) error {
 	return m.cache.SetString(c, masterID, key, data, ttl)
 }
 
-func (m *masterCache) HSet(c context.Context, companyID, subsidiaryID uint64, key string, field string, data interface{}, ttl time.Duration) error {
+func (m *masterCache) HSet(c context.Context, key string, field string, data interface{}, ttl time.Duration) error {
 	return m.cache.HSet(c, masterID, key, field, data, ttl)
 }
 
-func (m *masterCache) RPush(c context.Context, companyID, subsidiaryID uint64, key string, valueList []string) error {
+func (m *masterCache) RPush(c context.Context, key string, valueList []string) error {
 	return m.cache.RPush(c, masterID, key, valueList)
 }
 
-func (m *masterCache) IncrementValue(c context.Context, companyID, subsidiaryID uint64, key string) error {
+func (m *masterCache) IncrementValue(c context.Context, key string) error {
 	return m.cache.IncrementValue(c, masterID, key)
 }
 
-func (m *masterCache) SAdd(c context.Context, companyID, subsidiaryID uint64, key string, elements interface{}) error {
+func (m *masterCache) SAdd(c context.Context, key string, elements interface{}) error {
 	return m.cache.SAdd(c, masterID, key, elements)
 }
 
-func (m *masterCache) SRem(c context.Context, companyID, subsidiaryID uint64, key string, elements interface{}) error {
+func (m *masterCache) SRem(c context.Context, key string, elements interface{}) error {
 	return m.cache.SRem(c, masterID, key, elements)
 }
 
-func (m *masterCache) DelKey(c context.Context, companyID, subsidiaryID uint64, keys ...string) error {
+func (m *masterCache) DelKey(c context.Context, keys ...string) error {
 	return m.cache.DelKey(c, masterID, keys...)
 }
 
-func (m *masterCache) Expire(c context.Context, companyID, subsidiaryID uint64, key string, ttl time.Duration) error {
+func (m *masterCache) Expire(c context.Context, key string, ttl time.Duration) error {
 	return m.cache.Expire(c, masterID, key, ttl)
 }
 
