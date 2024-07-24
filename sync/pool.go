f286cd3bbed7fb8d98399dbe1ee9590335bfd1f5
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2024-07-24 17:49:55 +0900

    implement sync pool

---
+++ sync/pool.go
@@ -0,0 +1,162 @@
+// Package sync provides ways to execute multiple tasks concurrently
+// while synchronously managing them at the same time.
+package sync
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"runtime"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/retail-ai-inc/bean/v2"
+	"github.com/retail-ai-inc/bean/v2/internal/regex"
+	"github.com/sourcegraph/conc/panics"
+	"github.com/sourcegraph/conc/pool"
+)
+
+// Pool provides a way to execute multiple tasks concurrently and synchronously wait for all of them to finish.
+// It also recovers from panics within the tasks and support sentry tracing.
+type Pool struct {
+	pool *pool.ContextPool
+	span *sentry.Span
+}
+
+// NewPool creates a new pool with the given context and options.
+func NewPool(ctx context.Context, opts ...PoolOption) *Pool {
+
+	// set default options
+	pl := &poolOptions{
+		req: &http.Request{
+			Method: "",
+			URL:    &url.URL{},
+		},
+		max: 0,
+	}
+
+	for _, opt := range opts {
+		opt(pl)
+	}
+
+	// set setry transaction or span
+	var span *sentry.Span
+	if bean.BeanConfig.Sentry.On {
+		hub := sentry.GetHubFromContext(ctx)
+		if hub == nil {
+			hub = sentry.CurrentHub().Clone()
+		}
+
+		hub.Scope().SetRequest(pl.req)
+		ctx = sentry.SetHubOnContext(ctx, hub)
+
+		if bean.BeanConfig.Sentry.TracesSampleRate > 0.0 {
+			urlPath := pl.req.URL.Path
+
+			functionName := "unknown function"
+			if bean.BeanConfig.Sentry.On && bean.BeanConfig.Sentry.TracesSampleRate > 0.0 {
+				if pc, file, line, ok := runtime.Caller(1); ok {
+					functionName = fmt.Sprintf("%s:%d\n\t\r %s\n", path.Base(file), line, runtime.FuncForPC(pc).Name())
+				}
+			}
+			span = sentry.StartSpan(ctx, "sync",
+				sentry.WithTransactionName(fmt.Sprintf("%s %s SYNC", pl.req.Method, urlPath)),
+				sentry.ContinueFromRequest(pl.req),
+				sentry.WithDescription(functionName),
+			)
+
+			if regex.MatchAnyTraceSkipPath(urlPath) {
+				span.Sampled = sentry.SampledFalse
+			}
+		}
+	}
+
+	if span != nil {
+		ctx = span.Context()
+	}
+
+	var poolCtx *pool.ContextPool
+	if pl.max > 0 {
+		poolCtx = pool.New().WithErrors().WithContext(ctx).WithMaxGoroutines(pl.max)
+	} else {
+		poolCtx = pool.New().WithErrors().WithContext(ctx)
+	}
+
+	return &Pool{
+		pool: poolCtx,
+		span: span,
+	}
+}
+
+// PoolOption provides options to configure the pool.
+type PoolOption func(*poolOptions)
+
+type poolOptions struct {
+	max int
+	req *http.Request
+}
+
+// WithRequest sets a http request for sentry tracing.
+func WithRequest(req *http.Request) PoolOption {
+	return func(opts *poolOptions) {
+		opts.req = req
+	}
+}
+
+// WithMaxGoroutines sets the maximum number of goroutines that can be executed concurrently.
+func WithMaxGoroutines(max int) PoolOption {
+	return func(opts *poolOptions) {
+		opts.max = max
+	}
+}
+
+// Go executes a task concurrently.
+// It return a panic event as an error, not a panic if any.
+func (p *Pool) Go(f func(ctx context.Context) error) {
+	p.pool.Go(func(ctx context.Context) (err error) {
+		// recover the panic
+		var catcher panics.Catcher
+		defer func() {
+			if r := catcher.Recovered(); r != nil {
+				err = r.AsError()
+
+				if bean.BeanConfig.Sentry.On {
+					var localHub *sentry.Hub
+					if ctx != nil {
+						localHub = sentry.GetHubFromContext(ctx)
+					}
+					if localHub == nil {
+						localHub = sentry.CurrentHub().Clone()
+					}
+					localHub.ConfigureScope(func(scope *sentry.Scope) {
+						scope.SetTag("goroutine", "true")
+					})
+					localHub.Recover(err)
+				}
+			}
+		}()
+
+		// execute the task and catch panic if any
+		catcher.Try(func() {
+			err = f(ctx)
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+// Wait waits for all tasks to finish and get multiple joined errors if any.
+func (p *Pool) Wait() error {
+	defer func() {
+		if p.span != nil {
+			p.span.Finish()
+		}
+	}()
+
+	return p.pool.Wait()
+}
---
+++ sync/pool_test.go
@@ -0,0 +1,197 @@
+// MIT License
+
+// Copyright (c) The RAI Authors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Safe way to execute `go routine` without crashing the parent process while having a `panic`.
+package sync_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/retail-ai-inc/bean/v2"
+	"github.com/retail-ai-inc/bean/v2/sync"
+	"github.com/retail-ai-inc/bean/v2/trace"
+)
+
+func Test_Pool(t *testing.T) {
+
+	bean.BeanConfig = &bean.Config{
+		Sentry: bean.SentryConfig{
+			On:               true,
+			TracesSampleRate: 1.0,
+		},
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/hoge", nil)
+
+	task := func(c context.Context) error {
+		ctx, finish := trace.StartSpan(c, "task")
+		defer finish()
+
+		select {
+		case <-ctx.Done():
+			return context.Cause(ctx)
+		default:
+		}
+		dur := time.Duration(100) * time.Millisecond
+		fmt.Printf("task started for %v\n", dur)
+		time.Sleep(dur)
+		fmt.Println("task executed")
+
+		return nil
+	}
+
+	taskErr := func(c context.Context) error {
+		ctx, finish := trace.StartSpan(c, "task error")
+		defer finish()
+
+		select {
+		case <-ctx.Done():
+			return context.Cause(ctx)
+		default:
+		}
+
+		return errors.New("simulated task error")
+	}
+
+	taskPanic := func(c context.Context) error {
+		ctx, finish := trace.StartSpan(c, "task panic")
+		defer finish()
+
+		select {
+		case <-ctx.Done():
+			return context.Cause(ctx)
+		default:
+		}
+
+		panic("simulated task panic")
+	}
+
+	ctxTimeout, _ := context.WithTimeoutCause(
+		context.Background(),
+		time.Duration(50)*time.Millisecond,
+		errors.New("simulated context timeout"),
+	)
+
+	type args struct {
+		ctx   context.Context
+		opts  []sync.PoolOption
+		tasks []func(context.Context) error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "go tasks with request",
+			args: args{
+				ctx: context.Background(),
+				opts: []sync.PoolOption{
+					sync.WithRequest(req),
+				},
+				tasks: []func(context.Context) error{
+					task, task, task, task, task,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "go tasks with max less than tasks",
+			args: args{
+				ctx: context.Background(),
+				opts: []sync.PoolOption{
+					sync.WithMaxGoroutines(2),
+				},
+				tasks: []func(context.Context) error{
+					task, task, task, task, task,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "go tasks with errors",
+			args: args{
+				ctx:  context.Background(),
+				opts: nil,
+				tasks: []func(context.Context) error{
+					task, taskErr, task, task, taskErr,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "go tasks with panic",
+			args: args{
+				ctx:  context.Background(),
+				opts: nil,
+				tasks: []func(context.Context) error{
+					task, task, taskPanic, task, task,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "go tasks with timeout",
+			args: args{
+				ctx:  ctxTimeout,
+				opts: nil,
+				tasks: []func(context.Context) error{
+					task, task, task, task, task,
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pool := sync.NewPool(tt.args.ctx, tt.args.opts...)
+
+			for _, task := range tt.args.tasks {
+				pool.Go(task)
+			}
+
+			something := func(ctx context.Context) error {
+				_, finish := trace.StartSpan(ctx, "something")
+				defer finish()
+
+				dur := time.Duration(120) * time.Millisecond
+				fmt.Printf("something started for %v\n", dur)
+				time.Sleep(dur)
+				fmt.Println("something executed")
+
+				return nil
+			}
+			_ = something(tt.args.ctx)
+
+			err := pool.Wait()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GoPools() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
