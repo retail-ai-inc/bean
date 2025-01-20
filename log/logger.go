5acd3f293254e9a81eb7fa1ba3ce6fe1b8727e81
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2025-01-20 14:23:19 +0900

    feat: 🎸 create new logger func

--- log/logger.go
+++ log/logger.go
@@ -1,11 +1,29 @@
 package log
 
-import "github.com/labstack/echo/v4"
+import (
+	"sync"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
 
 // This is a global variable to hold the debug logger so that we can log data from service, repository or anywhere.
 var logger echo.Logger
+var mu sync.Mutex
+
+func New() echo.Logger {
+	if logger == nil {
+		mu.Lock()
+		logger = log.New("echo")
+		mu.Unlock()
+	}
+
+	return logger
+}
 
 func Set(l echo.Logger) {
+	mu.Lock()
+	defer mu.Unlock()
 	logger = l
 }
 
